0d545bab09cd859b3a9661a36980c7aaf912d612
Hannael-mich <thanna.lkuz@gmail.com>
2023-03-22 16:58:08 -0600

    modificacion de dos documentos

--- event.js
+++ event.js
@@ -31,3 +31,26 @@ boton.addEventListener('mouseover',function(){
 boton.addEventListener('mouseout',function(){
     boton.style.background="yellow";
 });
+
+//Focus
+var input = document.querySelector("#campo_nombre");
+
+input.addEventListener('focus',function(){
+    console.log("[focus] Estas dentro del input");
+});
+//Blur
+input.addEventListener('blur',function(){
+    console.log("[blur] Estas fuera del input]");
+});
+//Keydown
+input.addEventListener('keydown',function(event){
+    console.log("[keydown] Pulsando esta tecla",String.fromCharCode(event.keyCode));
+});
+//Keypress
+input.addEventListener('keypress',function(event){
+    console.log("[keypress] Tecla presionada",String.fromCharCode(event.keyCode));
+});
+//Keyup
+input.addEventListener('keyup',function(event){
+    console.log("[keyup] Tecla soltada",String.fromCharCode(event.keyCode));
+});
